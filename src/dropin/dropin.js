a8aedc5b07cc54d9a86b8097bd8d3c2c65d3a5fa
Baal89 <efestos89@gmail.com>
2021-05-13 15:13:10 +0100

    first commit

---
+++ src/dropin/dropin.js
@@ -0,0 +1,63 @@
+// 0. Get clientKey
+getClientKey().then(clientKey => {
+    getPaymentMethods().then(paymentMethodsResponse => {
+        // 1. Create an instance of AdyenCheckout
+        const checkout = new AdyenCheckout({
+            environment: 'test',
+            clientKey: clientKey, 
+            paymentMethodsResponse,
+            removePaymentMethods: ['paysafecard', 'c_cash'],
+            
+            onSubmit: (state, dropin) => {
+                // Global configuration for onSubmit
+                // Your function calling your server to make the `/payments` request
+                makePayment(state.data)
+                .then(response => {
+                  if (response.action) {
+                    dropin.handleAction(response.action);
+                  } else {
+                    
+                    if (response.resultCode == 'Authorised') {
+                      dropin.setStatus('success');
+                      dropin.setStatus('success', { message: `Payment successful! your reference number is: ${response.pspReference}` });
+                      } else if (response.resultCode == 'Refused') {
+                        dropin.setStatus('error');
+                        dropin.setStatus('error', { message: 'Something went wrong.'});
+                      }
+                    }
+                  })
+                  .catch(error => {
+                    throw Error(error);
+                  });
+              },
+              paymentMethodsConfiguration: {
+                card: { 
+                  hasHolderName: true,        
+                  holderNameRequired: true,
+                  enableStoreDetails: true,
+                  hideCVC: false, // Change this to true to hide the CVC field for stored cards
+                  name: 'Credit or debit card',
+                  billingAddressRequired: true,
+                  positionHolderNameOnTop: true,
+                  billingAddressAllowedCountries: true,
+                  data : {
+                    holderName: 'S. Hopper'
+                  },
+                  installmentOptions: {
+                    mc: {
+                        values: [1, 2, 3, 4]
+                    }
+                },
+                },
+              }
+        });
+        
+
+        // 2. Create and mount the Component
+        const dropin = checkout
+            .create('dropin', {
+                showStoredPaymentMethods: false
+            })
+            .mount('#dropin-container');
+    })
+});
